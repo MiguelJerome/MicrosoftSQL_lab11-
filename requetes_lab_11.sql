bd26e3fec7a31a2be5059f5918bd77da4d07772a
MiguelJerome <sharkmaycry@gmail.com>
2023-03-28 19:43:45 -0400

    add user_login

--- requetes_lab_11.sql
+++ requetes_lab_11.sql
@@ -1,4 +1,4 @@
---Revoquer tous les droits de l'utilisateur et supprimer tous les objets qui lui sont associes.
+-- Revoquer tous les droits de l'utilisateur et supprimer tous les objets qui lui sont associes.
 USE BD_Sales;
 GO
 REVOKE INSERT FROM user_nom_test
@@ -46,57 +46,112 @@ GO
 
 -- 2.	Creer une transaction permettant de supprimer les produits qui n'ont jamais ete commandes
 USE BD_Sales;
-PRINT @@TRANCOUNT  
-BEGIN TRAN  
-    PRINT @@TRANCOUNT 
-    -- Trouver les produits qui n'ont jamais été commandés
-    SELECT p.product_id
-    --Cette transaction cree une table temporaire #products_to_delete pour stocker les identifiants des produits qui n'ont jamais ete commandes
-    INTO #products_to_delete
-    FROM production.products p
-    LEFT JOIN sales.order_items oi ON p.product_id = oi.product_id
-    WHERE oi.product_id IS NULL; 
+PRINT @@TRANCOUNT;
+BEGIN TRAN;
+PRINT @@TRANCOUNT;
 
-    -- Supprimer les stocks correspondants
-    DELETE FROM production.stocks
-    WHERE product_id IN (SELECT product_id FROM #products_to_delete);
-    DELETE FROM production.products WHERE product_id NOT IN (SELECT product_id FROM sales.orders)
+-- Supprimer la table temporaire si elle existe deja
+IF OBJECT_ID('tempdb..#products_to_delete') IS NOT NULL
+  DROP TABLE #products_to_delete;
+-- Trouver les produits qui n'ont jamais été commandés
+SELECT p.product_id, p.product_name
+-- Cette transaction cree une table temporaire #products_to_delete pour stocker les identifiants des produits qui n ont jamais ete commandes
+INTO #products_to_delete
+FROM production.products p
+LEFT JOIN sales.order_items oi ON p.product_id = oi.product_id
+WHERE oi.product_id IS NULL
+ORDER BY p.product_id; 
 
-    -- Supprimer les produits correspondants
-    DELETE FROM production.products
-    WHERE product_id IN (SELECT product_id FROM #products_to_delete);
+-- vérifier que la table a bien été remplie avec les identifiants de produits qui n'ont jamais été commandés
+SELECT * FROM #products_to_delete
+ORDER BY product_id ASC;
 
-    -- Valider la transaction
-    COMMIT  
-    PRINT @@TRANCOUNT  
+-- Supprimer les stocks correspondants
+DELETE FROM production.stocks
+WHERE product_id IN (SELECT product_id FROM #products_to_delete);
+
+-- Supprimer les produits correspondants
+DELETE FROM production.products
+WHERE product_id IN (SELECT product_id FROM #products_to_delete);
+
+-- Vérifier que les produits ont bien été supprimés
+SELECT * FROM production.products;
+GO
+SELECT * FROM production.stocks;
+
+-- Valider la transaction
+COMMIT;
+PRINT @@TRANCOUNT;
+
+-- Annuler toutes les modifications apportées à la base de données depuis le début de la transaction en cours.
+ROLLBACK;
+PRINT @@TRANCOUNT;
+
+-- Vérifier que les produits ont bien été, annuler pour toutes les modifications apportées
+SELECT * FROM production.products;
+GO
+SELECT * FROM production.stocks;
+
+PRINT @@TRANCOUNT;
+
 
 -- 3.	Creer une procedure stockee affichant le montant des commandes comprise entre 50 et 300. 
--- Le prefixe "usp" signifie "User Stored Procedure"
-CREATE PROCEDURE [usp_GetOrderTotal]
-    /* Description: Retrieves the total amount of orders between 50 and 300. */
+USE BD_Sales
+GO
+
+CREATE PROCEDURE sp_GetOrdersBetween50And300
 AS
 BEGIN
-
---  SET NOCOUNT ON : Cela empechera SQL Server de renvoyer le nombre de lignes affectees apres chaque instruction, ce qui peut am�liorer les performances.
     SET NOCOUNT ON;
 
-    SELECT SUM(TotalAmount) AS OrderTotal
-    FROM [OrderTable]
-    WHERE TotalAmount BETWEEN 50 AND 300;
-
-    /* Returns: OrderTotal */
-    RETURN;
+    SELECT SUM(list_price * quantity) AS TotalAmount
+    FROM sales.order_items
+    WHERE list_price BETWEEN 50 AND 300;
 END
+GO
 
--- 4.	Creer une procedure stockee avec un parametre de sortie qui calcule et retourne le chiffre d'affaires.
-CREATE PROCEDURE [usp_GetRevenue]
-    @Revenue DECIMAL(18,2) OUTPUT
+EXEC sp_GetOrdersBetween50And300
+
+-- Supprimer la procédure stockée sp_GetOrdersBetween50And300de la base de données.
+USE BD_Sales
+GO
+
+DROP PROCEDURE sp_GetOrdersBetween50And300
+GO
+
+-- 4.	Créer une procédure stockée avec un paramètre de sortie qui calcule et retourne le chiffre d'affaires.
+USE BD_Sales;
+GO
+
+-- La procédure stockée prend deux dates en entrée
+-- et calcule le chiffre d'affaires entre ces deux dates en interrogeant les tables des commandes et des articles.
+-- La valeur calculée est stockée dans une variable de sortie @revenue de type DECIMAL(10, 2).
+CREATE PROCEDURE CalculateSalesRevenue
+@startDate DATE,
+@endDate DATE,
+@revenue DECIMAL(10, 2) OUTPUT
 AS
 BEGIN
-    SELECT @Revenue = SUM(OrderTotal)
-    FROM (
-        SELECT SUM(TotalAmount) AS OrderTotal
-        FROM OrderTable
-        GROUP BY OrderDate
-    ) AS Orders
-END
+SET NOCOUNT ON;
+SELECT oi.order_id, oi.product_id, oi.list_price, oi.quantity, (oi.list_price * oi.quantity) AS line_total, o.order_date
+FROM sales.order_items oi
+JOIN sales.orders o ON oi.order_id = o.order_id
+WHERE o.order_date >= @startDate AND o.order_date <= @endDate;
+
+SELECT @revenue = SUM(oi.list_price * oi.quantity)
+FROM sales.order_items oi
+JOIN sales.orders o ON oi.order_id = o.order_id
+WHERE o.order_date >= @startDate AND o.order_date <= @endDate;
+END;
+GO
+
+-- DECLARE @revenue DECIMAL(10, 2)
+-- exécute la procédure stockée CalculateSalesRevenue en utilisant deux dates comme paramètres d'entrée 
+-- et stocke le chiffre d'affaires calculé dans la variable @revenue,
+-- puis affiche la valeur stockée dans la variable @revenue
+DECLARE @revenue DECIMAL(10, 2);
+EXEC CalculateSalesRevenue '2016-01-01', '2016-01-31', @revenue OUTPUT;
+SELECT @revenue;
+
+-- Supprimer la procédure stockée CalculateSalesRevenue de la base de données.
+DROP PROCEDURE CalculateSalesRevenue;
